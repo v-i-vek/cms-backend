005bbaf6ac8e9d578fc25a993e707a7a418e09f3
kamlesh.kalal <kamlesh.kalal@volansys.com>
2023-04-05 18:10:13 +0530

    error sol

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./db/conn");
-const siteRoute = require('./route/siteManegement.route')
+
 const route = require("./route/materialRoute");
 const routequantity = require("./route/QuantityRoute");
-<<<<<<< HEAD
+
 const addressRoute = require('./route/address.user.route')
-const port = process.env.PORT || 9050;
-=======
+ const port = process.env.PORT || 3001;
+
 const docs = require("./models/DocumentModel");
 const routePermission = require('./route/permission.route')
 const document_master = require("./models/DocumentModel");
 const ServiceRoute = require("./route/ServiceRoute")
-const userroute = require("./route/user");
+// const userroute = require("./route/user");
 const roleRoute = require("./route/role");
 const siteRoute = require('./route/siteManegement.route')
-const addressRoute = require('./route/address.user.route')
-const port = process.env.PORT || 9100;
->>>>>>> master
+
 app.use(express.json());
 const DB_URL = "mongodb+srv://root:root@cluster0.63corsg.mongodb.net/test";
 connectDb(DB_URL);
-app.use(userroute);
+//app.use(userroute);
 app.use(roleRoute)
 app.use("/uploads", express.static("uploads"));
 app.use(route);
