92dd88b16ddc34741cff8768bbdb6cfcc4cf8622
kamlesh.kalal <kamlesh.kalal@volansys.com>
2023-04-24 12:19:49 +0530

    login

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/registerUser");
+const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const userLoginGet = async (req, res) => {
@@ -6,26 +7,41 @@ const userLoginGet = async (req, res) => {
   res.status(201).send(getdata);
 };
 const userafterLoginGet = (req, res) => {
-  console.log(`this is the cookiew ${req.cookies.jwt}`);
+  // console.log(`this is the cookiew ${req.cookies.jwt}`);
   res.render("secret");
 };
 
 const userLoginPost = async (req, res) => {
   try {
+     
     const email = req.body.email;
     const password = req.body.password;
+    console.log('this is the cookiew ');
     const dbmail = await userModel.findOne({ email: email });
+    //console.log(dbmail);
     // bcrypt hash is checked in the database using the compare
-    const isMatch = bcrypt.compare(password, dbmail.password);
+    const isMatch = await bcrypt.compare(password, dbmail.password);
+    console.log(isMatch)
     //generating tokens
+
     const token = await dbmail.generateAuthToken();
-    console.log("token", token);
+    console.log("token>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", token);
     // adding cookies
-    res.cookie("jwt", token, {
-      expires: new Date(Date.now() + 30000)
-    });
+  
+
+
+    console.log('working1 ')
+
+
+    let data;
     if (isMatch) {
-      return res.status(201).send({ message: "successful" });
+      const user = jwt.sign({email:email},token)
+      console.log("  USERS :-    "+user);
+      data = {dbmail};
+      data.user = user;
+     console.log(data)
+      return res.status(201).send(data);
+    
     } else {
       return res.send("invalid login details");
     }
