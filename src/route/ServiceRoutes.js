65c7cff0f70d546024488aa11d88cb519c7eb747
bansariprajapati <bansariprajapati09@gmail/com>
2023-04-28 15:28:49 +0530

    populate

--- src/route/ServiceRoutes.js
+++ src/route/ServiceRoutes.js
@@ -1,13 +1,13 @@
 const express = require ("express");
-const router = express.Router();
-const SerController = require('../controllers/ServiceController');
-const upload = require('../middleware/upload')
+const ServiceRoute = express.Router();
+const SerController = require('../controller/ServiceController')
+const upload = require('../middleware/serviceupload')
 
-// router.post('/services',SerController.createSer);
-router.post('/services',upload.single('serviceimage'),SerController.createSer)
-router.get('/services/:id', SerController.getSer);
-router.patch('/services/:id', SerController.updateSer);
-router.delete('/services/:id',SerController.deleteSer);
-router.get('/service' , SerController.getAll)
 
-module.exports = router;
+ServiceRoute.post('/services',upload.single('serviceimage'),SerController.createSer)
+ServiceRoute.get('/services/:id', SerController.getSer);
+ServiceRoute.patch('/services/:id', SerController.updateSer);
+ServiceRoute.delete('/services/:id',SerController.deleteSer);
+ServiceRoute.get('/service' , SerController.getAll)
+
+module.exports = ServiceRoute;
