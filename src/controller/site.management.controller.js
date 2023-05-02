7ec24a1ee73313f675c77a767aa6a7a79eb230c8
v-i-vek <vivek.yadav@volansys.com>
2023-05-02 16:53:22 +0530

    img is uploading

--- src/controller/site.management.controller.js
+++ src/controller/site.management.controller.js
@@ -1,22 +1,19 @@
-const {siteMangementModel} = require("../models/site.management");
-
-
-
+const { siteMangementModel } = require("../models/site.management");
 
 // this is the get Method for reading the data for only one user
 const siteGet = async (req, res) => {
   const result = await siteMangementModel.findOne(req.params.email);
-  res.status(201).send(result)
+  res.status(201).send(result);
 };
 // getting all the data
 const siteAllGet = async (req, res) => {
   try {
     const Flat = await siteMangementModel.find();
-    Flat.forEach((obj)=>{
-          obj.flatDetails = obj.flatDetails.filter((data)=>{
-              return data.isBought === false
-          })
-      })
+    Flat.forEach((obj) => {
+      obj.flatDetails = obj.flatDetails.filter((data) => {
+        return data.isBought === false;
+      });
+    });
     return res.status(201).send(Flat);
   } catch (error) {
     return res.status(401).send(error);
@@ -28,97 +25,83 @@ const sitePatch = async (req, res) => {
     const id = req.params.id;
     const result = await siteMangementModel.findById({ _id: id });
     noOfFloor = req.body.noOfFloor;
-    noOfFlatPerFloor = req.body.noOfFlatPerFloor
-    totalFlat = noOfFloor*noOfFlatPerFloor
-        
-    let flats = []
-    let numberOfFlats = countFlat(noOfFloor,noOfFlatPerFloor,flats)
-    
+    noOfFlatPerFloor = req.body.noOfFlatPerFloor;
+    totalFlat = noOfFloor * noOfFlatPerFloor;
+
+    let flats = [];
+    let numberOfFlats = countFlat(noOfFloor, noOfFlatPerFloor, flats);
+
     const update = await result.updateOne({
       name: req.body.name,
       siteId: req.body.id,
       location: req.body.location,
-      noOfFloor:req.body.noOfFloor,
-      noOfFlatPerFloor:req.body.noOfFlatPerFloor,
-      totalFlat : totalFlat,
-      flatDetails:numberOfFlats
+      noOfFloor: req.body.noOfFloor,
+      noOfFlatPerFloor: req.body.noOfFlatPerFloor,
+      totalFlat: totalFlat,
+      flatDetails: numberOfFlats,
     });
-    res.status(201).send({message:"successful"});
+    res.status(201).send({ message: "successful" });
   } catch (error) {
-    res.status(401).send({message:error});
+    res.status(401).send({ message: error });
   }
 };
 // this method is to delete the data from the db
 const siteDelete = async (req, res) => {
   try {
     const id = req.params.id;
-    const deleteAddress = await siteMangementModel.findByIdAndDelete({ _id: id,});
-   return  res.status(201).send(deleteAddress);
+    const deleteAddress = await siteMangementModel.findByIdAndDelete({
+      _id: id,
+    });
+    return res.status(201).send(deleteAddress);
   } catch (error) {
-   return res.status(401).send(error.message)
+    return res.status(401).send(error.message);
   }
 };
 // this is for posting the data in db through post method
 const sitePost = async (req, res) => {
   try {
-    
     noOfFloor = req.body.noOfFloor;
-    noOfFlatPerFloor = req.body.noOfFlatPerFloor
-    totalFlat = noOfFloor*noOfFlatPerFloor
-    
-    let flats = []
-    let numberOfFlats = countFlat(noOfFloor,noOfFlatPerFloor,flats)
-    console.log(numberOfFlats)
+    noOfFlatPerFloor = req.body.noOfFlatPerFloor;
+    totalFlat = noOfFloor * noOfFlatPerFloor;
+
+    let flats = [];
+    let numberOfFlats = countFlat(noOfFloor, noOfFlatPerFloor, flats);
+    console.log(numberOfFlats);
 
     const siteManage = new siteMangementModel({
       siteName: req.body.siteName,
       category: req.body.category,
       location: req.body.location,
-      noOfFloor:req.body.noOfFloor,
-      noOfFlatPerFloor:req.body.noOfFlatPerFloor,
-      totalFlat : totalFlat,
-      flatDetails:numberOfFlats
+      noOfFloor: req.body.noOfFloor,
+      noOfFlatPerFloor: req.body.noOfFlatPerFloor,
+      totalFlat: totalFlat,
+      flatDetails: numberOfFlats,
     });
-    
+
     if (req.file) {
       siteManage.brandLogo = req.file.path; // this is for checking the valid name and giving the path
     }
     const saved = await siteManage.save();
     return res.status(200).send(saved);
   } catch (error) {
-  return  res.status(401).send(error.message)
+    return res.status(401).send(error.message);
   }
 };
 
-function countFlat(noOfFloor,noOfFlatPerFloor,flats){
-  let temp =100
-    for(let i = 1;i<=noOfFloor;i++){
-        let count = temp;
-        for(let j = 1;j<=noOfFlatPerFloor;j++){
-          count++
-          let flat = new Object()
-          flat.flatNo= count,
-          flat.userId=null,
-          flat.isBought=false
-          flats.push(flat)
-        }
-        temp =0;
-        temp = (i+1)*100 
-      }
-      return flats
+function countFlat(noOfFloor, noOfFlatPerFloor, flats) {
+  let temp = 100;
+  for (let i = 1; i <= noOfFloor; i++) {
+    let count = temp;
+    for (let j = 1; j <= noOfFlatPerFloor; j++) {
+      count++;
+      let flat = new Object();
+      (flat.flatNo = count), (flat.userId = null), (flat.isBought = false);
+      flats.push(flat);
+    }
+    temp = 0;
+    temp = (i + 1) * 100;
+  }
+  return flats;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = { sitePost, siteGet, sitePatch, siteDelete, siteAllGet };
